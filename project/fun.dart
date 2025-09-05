416f7e235e0df020b4f3523886bf69466ef93fe7
Ebram  Wagdy Samy Zaki <wagdyebram891@gmail.com>
2025-09-06 02:57:43 +0300

    Update project files

---
+++ project/fun.dart
@@ -0,0 +1,80 @@
+import 'dart:io';
+
+/// Function to print the game board in a formatted way
+/// If the cell is empty, it shows the cell number
+/// If the cell is not empty, it shows the player symbol (X or O)
+void printBoard(List<String>board){
+
+  // check if cell empty print num of cell if not print text
+  String cell(int i)=>board[i].isEmpty ? "${i + 1}" : board[i];
+
+  print("""
+╔═════════╦═════════╦═════════╗
+║   ${cell(0)}     ║    ${cell(1)}    ║    ${cell(2)}    ║
+╠═════════╬═════════╬═════════╣
+║   ${cell(3)}     ║    ${cell(4)}    ║    ${cell(5)}    ║
+╠═════════╬═════════╬═════════╣
+║   ${cell(6)}     ║    ${cell(7)}    ║    ${cell(8)}    ║
+╚═════════╩═════════╩═════════╝
+     """);
+}
+
+
+
+/// Function to handle a player's move
+/// Asks the player to choose a position [1-9]
+/// Validates the input (must be a number within the range)
+/// Checks if the cell is free
+/// Updates the board with the player's char
+void movePlayer(List<String>board, String player , String char){
+  while(true){
+    print("$player($char) please chose position from [1-9] :");
+    int? pos=int.tryParse(stdin.readLineSync()??"");
+    if(pos==null || pos<1 || pos>9){
+      print("Invalid Input Try again");
+      continue;
+    }
+    else if(board[pos-1]!=''){
+      print("Cell Already Taken");
+      continue;
+    }
+    else{
+      board[pos-1]=char;
+      break;
+    }
+  }
+}
+
+
+
+/// Function to check the winner
+/// Contains All possibilities to win
+/// Returns "X" or "O" if a player wins
+/// Returns "D" if the board is full with no winner (draw)
+/// Returns null if the game is still work and not player is win
+String? checkWinner(List<String>b){
+
+  const win=[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for(var w in win){
+    String a= b[w[0]],c=b[w[1]],d=b[w[2]];
+    if(a!="" && a==c && c==d){
+      return a;
+    }
+  }
+  if(!b.contains("")){
+    return "D";
+  }
+  else{
+    return null;
+  }
+
+}
---
+++ project/game.dart
@@ -0,0 +1,96 @@
+import 'dart:io';
+import 'fun.dart';
+
+void main()
+{
+
+  //Add player name
+
+  print("Please Enter your Player 1 name :");
+  String p1name=stdin.readLineSync()!;
+
+  print("Please Enter your Player 2 name :");
+  String p2name=stdin.readLineSync()!;
+
+  //chose play x or o
+  print("Hi $p1name chose your character X or O :");
+  String Choice= (stdin.readLineSync()! .toLowerCase());
+  String p1char=(Choice=="x")? "x":"o" ;
+  String p2char=(Choice=="x")? "o":"x" ;
+
+  //initialize Default score
+  int p1score=0 ,p2sccore=0;
+
+  String currentPlayer = p1name;
+  String currentChar = p1char;
+
+  //initialize board as  a List
+  List <String> board=List.filled(9, "");
+
+  while(true)
+  {
+    // setup game and switch player
+    print("Game Start".padLeft(20));
+    print("$p1name:$p1char                     $p2name:$p2char");
+    printBoard(board);
+    movePlayer(board, currentPlayer, currentChar);
+    if (currentPlayer == p1name) {
+      currentPlayer = p2name;
+      currentChar = p2char;
+    } else {
+      currentPlayer = p1name;
+      currentChar = p1char;
+    }
+
+    // Check If Player is win or Draw or the game is still return null
+    String? winner = checkWinner(board);
+    if (winner != null) {
+
+       printBoard(board);
+
+      if (winner == "x" || winner == "o") {
+        String winnerName = (winner == p1char) ? p1name : p2name;
+        print("$winnerName WINS!🎉🎉");
+        //check and add score
+        if (winner == p1char) {
+          p1score++;
+        }
+        else if (winner == p2char) {
+          p2sccore++;
+        }
+      }
+      else if (winner == "D") {
+        print("It's a Draw!🤝🤝");
+      }
+
+      //print score
+      print("Score => $p1name: $p1score | $p2name: $p2sccore");
+
+      //ask if you want to play again or not
+      print("Do you want to play again? (y/n): ");
+      String again = stdin.readLineSync()!.toLowerCase();
+
+      //check if want play again or not
+      if (again != "y") {
+        print("Thanks for playing 👋👋");
+        break;
+      }
+      else{
+        board = List.filled(9, "");
+        currentPlayer = p1name;
+        currentChar = p1char;
+        continue;
+      }
+
+
+
+    }
+
+
+  }
+}
+
+
+
+
+
